14f6edb80df72b3c0ab27fe600b39b5f6778fb50
Nikita Melikhov <nmelikhov@unisnet.ru>
2011-06-23 11:30:27 +0400

    Initial commit

---
+++ UTM5-URFAClient-Daemon/lib/UTM5/URFAClient/Daemon.pm
@@ -0,0 +1,78 @@
+package UTM5::URFAClient::Daemon;
+
+use v5.10;
+use strict;
+use warnings;
+
+=head1 NAME
+
+UTM5::URFAClient - Perl wrapper for Netup URFA Client
+
+=head1 VERSION
+
+
+Version 0.1
+
+=cut
+
+our $VERSION = '0.1';
+
+=head1 SYNOPSIS
+
+=cut
+
+=head1 SUBROUTINES/METHODS
+
+
+=head2 user_list
+
+=cut
+
+=head1 AUTHOR
+
+Nikita Melikhov, C<< <ver at 0xff.su> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-utm5-urfaclient at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=UTM5-URFAClient>.
+I will be notified, and then you'll automatically be notified of progress on your bug as I make changes.
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc UTM5::URFAClient
+
+
+You can also look for information at:
+
+=over 4
+
+=item * Netup official site
+
+L<http://www.netup.ru/>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=UTM5-URFAClient>
+
+=back
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Nikita Melikhov.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of UTM5::URFAClient
